ba4c8cf186e92eceee1dc1615297a52db525b2a4
UtsavCodes-UC <upatel2107.up@gmail.com>
2025-09-22 17:09:23 +0530

    Port updated for deployement - v1

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
 import pandas as pd
 import random
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -44,4 +45,5 @@ def get_puzzle():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
